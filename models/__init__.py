326001f87f440b39d7d6b5218c0ad37e27f549b6
Ishant Shrivastava <116137467+KIshantS2525@users.noreply.github.com>
2025-03-28 11:32:11 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -43,16 +43,16 @@ def from_pretrained(path: str, **kwargs):
     from safetensors.torch import load_file
     is_local = os.path.exists(f"{path}.json") and os.path.exists(f"{path}.safetensors")
 
-    if is_local:
-        config_file = f"{path}.json"
-        model_file = f"{path}.safetensors"
-    else:
-        from huggingface_hub import hf_hub_download
-        path_parts = path.split('/')
-        repo_id = f'{path_parts[0]}/{path_parts[1]}'
-        model_name = '/'.join(path_parts[2:])
-        config_file = hf_hub_download(repo_id, f"{model_name}.json")
-        model_file = hf_hub_download(repo_id, f"{model_name}.safetensors")
+    # if is_local:
+    config_file = f"{path}.json"
+    model_file = f"{path}.safetensors"
+    # else:
+    #     from huggingface_hub import hf_hub_download
+    #     path_parts = path.split('/')
+    #     repo_id = f'{path_parts[0]}/{path_parts[1]}'
+    #     model_name = '/'.join(path_parts[2:])
+    #     config_file = hf_hub_download(repo_id, f"{model_name}.json")
+    #     model_file = hf_hub_download(repo_id, f"{model_name}.safetensors")
 
     with open(config_file, 'r') as f:
         config = json.load(f)
